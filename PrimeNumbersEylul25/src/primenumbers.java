692de5b44c654d5955ff501e484b4bd0841effb8
20090104019 <20090104019@muhlab11.lab.khas.edu.tr>
2012-09-25 08:30:32 -0430

    prime numbers ilk hali

---
+++ PrimeNumbersEylul25/src/primenumbers.java
@@ -0,0 +1,18 @@
+
+public class primenumbers {
+
+	
+	public static void main(String[] args) {
+		int a=3;  
+        int b=4;
+        int d=7;
+        int f=25;
+        double c=Math.sqrt((a*a)+(b*b));
+        double e=Math.sqrt((f*f)-(d*d));
+        System.out.println("Kenarlarý " + a + " ve " + b + " olan üçgenin hipotenüsü = "+ c);
+        
+	
+
+	}
+
+}
