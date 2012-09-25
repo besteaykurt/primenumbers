a847dcca5a57f050fa9fdf49378c3894452d8f6b
20090104019 <20090104019@muhlab11.lab.khas.edu.tr>
2012-09-25 08:46:51 -0430

    for ve if ler

--- PrimeNumbersEylul25/src/primenumbers.java
+++ PrimeNumbersEylul25/src/primenumbers.java
@@ -2,17 +2,26 @@
 public class primenumbers {
 
 	
-	public static void main(String[] args) {
-		int a=3;  
-        int b=4;
-        int d=7;
-        int f=25;
-        double c=Math.sqrt((a*a)+(b*b));
-        double e=Math.sqrt((f*f)-(d*d));
-        System.out.println("Kenarları " + a + " ve " + b + " olan üçgenin hipotenüsü = "+ c);
-        
+	
+		static int number;
+		static int a=68;  
+		static public int IsPrime(int number)	{
+		if (number == 1 || number == 2)	
+			return 1;
+		
+		for (int i=2; i<a; i++)	{
+		if ( (number/i)==(int)(number/i) )
+		return 0;
+		}
+		return 1;
+		
+		}
+
+		public static void main(String[] args) {
+        System.out.println("Asal sayılar = " + number);
+		}
 	
 
-	}
+	
 
 }
